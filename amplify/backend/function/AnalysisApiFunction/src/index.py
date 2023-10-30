8da1c493176109cabab0df0677468258b050da1c
masaki <lasalle4615@icloud.com>
2023-10-31 01:22:21 +0900

    fastapi風に＋requirements.txtの追加

--- amplify/backend/function/AnalysisApiFunction/src/index.py
+++ amplify/backend/function/AnalysisApiFunction/src/index.py
@@ -1,15 +1,12 @@
 import json
+from fastapi import FastAPI
+from mangum import Mangum
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
 
 def handler(event, context):
-  print('received event:')
-  print(event)
-  
-  return {
-      'statusCode': 200,
-      'headers': {
-          'Access-Control-Allow-Headers': '*',
-          'Access-Control-Allow-Origin': '*',
-          'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
-      },
-      'body': json.dumps('Hello from your new Amplify Python lambda!')
-  }
+  return Mangum(app)(event, context)
