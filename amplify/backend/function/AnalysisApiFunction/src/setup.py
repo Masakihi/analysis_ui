0c5750ac8bd0956c99a42cc351310c2712f0c5e2
masaki <lasalle4615@icloud.com>
2023-10-31 13:52:39 +0900

    今度こそamplify pushできた

--- amplify/backend/function/AnalysisApiFunction/src/setup.py
+++ amplify/backend/function/AnalysisApiFunction/src/setup.py
@@ -1,3 +1,17 @@
 from distutils.core import setup
 
-setup(name='src', version='1.0')
+setup(
+    name='src',
+    version='1.0',
+    install_requires=[
+        "fastapi",
+        "uvicorn",
+        "mangum",
+        "numba",
+        "numpy",
+        "matplotlib",
+        "fabio",
+        "scipy",
+        "python-multipart"
+    ]
+)
